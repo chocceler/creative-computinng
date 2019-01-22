32efd4e80cc83393eec3d728a26673779c4a16a2
Lilliana Checo <checl024@newschool.edu>
2019-01-22 14:04:38 -0500

    for test

--- mal/emailform.php
+++ mal/emailform.php
@@ -1,34 +1,48 @@
 <?php
 $errors = '';
-$myemail = 'lil.checo9@gmail.com';//<-----Put Your email address here.
-if(empty($_POST['name'])  ||
-   // empty($_POST['email']) ||
-   empty($_POST['message']))
+$mailTo = "lilliana@checo.design";//<-----Put Your email address here.
+// xx@mms.us.lycamobile.com
+if(empty($_POST['message'])  ||
+
 {
     $errors .= "\n Error: all fields are required";
 }
-$name = $_POST['name'];
-// $email_address = $_POST['email'];
-$message = $_POST['message'];
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
-
+// $name = $_POST['name'];
+//
+// if (!preg_match(
+// "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+// $email_address))
+// {
+//     $errors .= "\n Error: Invalid email address";
+// }
+//
 if( empty($errors))
 {
-$to = $myemail;
-$email_subject = "Contact form submission: $name";
-$email_body = "You have received a new message. ".
-" Here are the details:\n Name: $name \n ".
-// "Email: $email_address\n
-//  Message \n $message";
-// $headers = "From: $myemail\n";
-// $headers .= "Reply-To: $email_address"
-// mail($to,$email_subject,$email_body,$headers);
-redirect to the 'thank you' page
-header('Location: contact-form-thank-you.html');
+  $headers = "From: melodianouta website";
+    $txt = "Song rec: ".$message.
+
+    mail($mailTo, $txt, $headers);
+    header("Location: localhost/melodianouta.php?mailsend")
+// $to = $myemail;
+// $email_subject = "Contact form submission: $name";
+// $email_body = "You have received a new message. ".
+// " Here are the details:\n Name: $name \n ".
+// // "Email: $email_address\n
+// //  Message \n $message";
+// // $headers = "From: $myemail\n";
+// // $headers .= "Reply-To: $email_address"
+// // mail($to,$email_subject,$email_body,$headers);
+// redirect to the 'thank you' page
+// header('Location: contact-form-thank-you.html');
 }
+
+// if (isset($_POST['submit'])) {
+//   $name = $_POST['name'];
+//
+//   $mailTo = "lil.checo9@gmail.com";
+//   $headers = "From: melodianouta website";
+//   $txt = "Song rec: ".$name.
+//
+
+// }
 ?>
